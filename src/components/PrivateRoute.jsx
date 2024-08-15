32efcca9d5e14d01f3e35c47082a684b43a8dc8a
Premved Dhote <premveddhote@gmail.com>
2024-08-15 23:41:26 +0530

    Authentication with reset password

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
-import { Route , Redirect } from 'react-router-dom'
-import {useAuth} from '../contexts/AuthContext'
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
-function PrivateRoute({component: Component, ...rest}) {
-const currentUser = useAuth()
+function PrivateRoute() {
+  const currentUser = useAuth();
 
-  return (
-    <Route 
-    {...rest} render={(props) => {
-     return  currentUser ? <Component {...props} /> : <Redirect to="/login" />
-    }} >
-
-    </Route>
-  )
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
 }
 
-export default PrivateRoute
+export default PrivateRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import Destinations from "./components/Destinations.jsx";
-import OurServices from "./components/OurServices.jsx";
-import Testimonials from "./components/Testimonials.jsx";
-import Footer from "./components/Footer.jsx";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext.jsx";
 import Layout from "./components/Layout.jsx";
 import Home from "./components/Home.jsx";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
 import About from "./components/about.jsx";
-import AuthProvider from "./contexts/AuthContext.jsx";
+import Destinations from "./components/Destinations.jsx";
+import OurServices from "./components/OurServices.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import ForgotPassword from "./components/ForgotPassword.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <PrivateRoute path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="destinations" element={<Destinations />} />
-      <Route path="ourservices" element={<OurServices />} />
+    <Route path="/" element={<Layout />}>
+      <Route element={<PrivateRoute />}>
+        <Route path="" element={<Home />} />
+        <Route path="destinations" element={<Destinations />} />
+        <Route path="ourservices" element={<OurServices />} />
+      </Route>
       <Route path="signup" element={<Signup />} />
       <Route path="login" element={<Login />} />
       <Route path="about" element={<About />} />
-    </PrivateRoute>
+      <Route path="ForgotPassword" element={<ForgotPassword />} />
+    </Route>
   )
 );
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
   <AuthProvider>
     <RouterProvider router={router} />
   </AuthProvider>
-  // {/* </StrictMode> */}
 );
